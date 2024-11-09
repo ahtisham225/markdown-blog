d7189da4c805cf945b37c77e644744d64946e0c8
ahtisham225 <ahsitham225ali@gmail.com>
2024-11-09 16:04:19 +0500

    Lucky 97 Site Ready

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,1 @@
-const mode = localStorage.getItem("mode") || "";
-const toggle = document.querySelector(".toggle");
-const body = document.querySelector("body");
-
-document.body.className = mode;
-
-toggle.addEventListener("click", ()=>{
-  localStorage.setItem("mode", mode === "light" ? "" : "light")
-  body.classList.toggle("light")
-})
+const mode=localStorage.getItem("mode")||"",toggle=document.querySelector(".toggle"),body=document.querySelector("body");function toggleMenu(){let e=document.querySelector(".navbar");e.classList.toggle("open"),e.classList.contains("open")?document.addEventListener("click",closeMenuOnOutsideClick):document.removeEventListener("click",closeMenuOnOutsideClick)}function closeMenuOnOutsideClick(e){let t=document.querySelector(".navbar"),o=document.querySelector(".menu-icon");t.contains(e.target)||o.contains(e.target)||(t.classList.remove("open"),document.removeEventListener("click",closeMenuOnOutsideClick))}document.body.className=mode,toggle.addEventListener("click",()=>{localStorage.setItem("mode","light"===mode?"":"light"),body.classList.toggle("light")});
